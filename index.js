1e0f4ca33c5640a1a61300780474be4b54faea4e
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-07-02 19:53:43 +0200

    beautify js

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ var fs = require('fs');
 
 var server = http.createServer();
 
-server.on('request', function (request, response) {
+server.on('request', function(request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
         response.write('Odczyt Index.js:');
         fs.readFile('./index.js', 'utf-8', function(err, data) {
-    		response.write(data);
- 			response.end();
-});
-           
-    } else {
-            response.statusCode = 404;
-            Response.Write(<img src="error.jpg" alt="error">
-);
+            response.write(data);
             response.end();
+        });
+        
+    } else {
+        response.statusCode = 404;
+        Response.Write( < img src = "error.jpg"
+            alt = "error" >
+        );
+        response.end();
     }
 });
 
